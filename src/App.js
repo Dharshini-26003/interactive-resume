c6c5ed7accd314c3a3c6057cbfe97c3d1151bf2f
Dharshini-26003 <dharshuvj2611@gmail.com>
2025-08-02 04:21:13 +0530

    Added About Section with personal info and front end focus

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react"
 import MyProfile from './components/myprofile'
 import ProfilePic from "./sidebar/ProfilePic";
 import Icons from "./sidebar/socialicons";
+import About from "./components/about"; 
+import MySkills from "./components/skills";
 import './styles/App.css';
 import './styles/index.css'
 import './styles/resumeintro.css';
+import './styles/about.css';
 
 
 function App() {
@@ -14,6 +17,12 @@ function App() {
         <ProfilePic />
         <MyProfile />
         <Icons />
+      </div>
+      <div className="about-section">
+        <About />
+      </div>
+      <div className="skills-section">
+        <MySkills />
       </div>
     </div>
   );
---
+++ src/components/skills.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function MySkills(){
+    return (
+        <div className='skills-sec'>
+        <h2>Skills</h2>
+        <ul className='skills-list'>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>ReactJS</li>
+            <li>Bootstarp</li>
+            <li>Responsive Design</li>
+            <li>UI/UX Design</li>
+            <li>GitHub</li>
+        </ul>
+        </div>
+    );
+}
+export default MySkills;
