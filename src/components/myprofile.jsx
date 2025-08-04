a189be990fbad97e6d9a1e31fd855ec48702308e
Dharshini-26003 <dharshuvj2611@gmail.com>
2025-08-04 05:40:02 +0530

    Update myprofile.jsx

--- src/components/myprofile.jsx
+++ src/components/myprofile.jsx
@@ -2,14 +2,8 @@ import React from 'react'
 function MyProfile() {
   return (
     <div className='profile' id='home'>
-<<<<<<< HEAD
       <h2>Hi! I'm Dharshini, Welcome to my profile...</h2>
   
-=======
-      <h2>Hi! I'm Dharshini</h2>
-      <h2>Welcome to my profile...!!!</h2>
-
->>>>>>> 1d73f7cf56318f9807b7880f6e544be7ae6d20d2
 <button onClick={() => {
   document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
 }} className='knowmore'>
